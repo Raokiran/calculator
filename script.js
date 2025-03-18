46031455a8a1f7765ccc6e8a56a367c1848ceb6d
Rao Kiran <125569455+Raokiran@users.noreply.github.com>
2025-03-18 10:20:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+let exp = "";
+let post = [];
+let digitExpression = /^[-+]?\d{1,7}(\.\d{0,1})?$/;
+
+function display(ex) {
+  document.getElementById("value").innerHTML = ex;
+  exp = "";
+}
+
+function val(value) {
+  if (post.length < 50) {
+    if (value === "AC") {
+      exp = "";
+      post = [];
+      display(exp);
+    } else if (value === "+/-") {
+      if (digitExpression.test(post[post.length - 1])&&!digitExpression.test(post[post.length - 2])) {
+        let digit = post.pop();
+        if (digit.startsWith("-")) {
+          digit = digit.substring(1);
+        } else {
+          digit = "-" + digit;
+        }
+        post.push(digit);
+        exp = post.join("");
+        display(exp);
+      }
+    } else if (value === "=") {
+      if (post.length === 0) {
+        exp = "Please enter the value";
+      } else if (
+        ["+", "-", "/", "%", ".", "x"].includes(post[post.length - 1])
+      ) {
+        post = [];
+        exp = "Invalid Expression";
+      } else {
+        exp = calculation(post);
+        post = [];
+      }
+      display(exp);
+    } else {
+      if (
+        [ "/", "%", "x"].includes(post[post.length - 1]) &&
+        [ "/", "%", "x"].includes(value)
+      ) 
+      {
+        post.pop();
+      }
+       else if (value === "-") 
+        {
+        if (
+          post.length === 0 ||
+          ["+", "-", "/", "%", "x"].includes(post[post.length - 1])
+        ) {
+          post.push(value);
+        } else {
+          post.push(value);
+        }
+      } 
+      else if (!isNaN(value) || value === ".") {
+        if (
+          !isNaN(post[post.length - 1]) ||
+          post[post.length - 1]?.includes(".")
+        ) {
+          post[post.length - 1] += value;
+        } 
+         else {
+          post.push(value);
+        }
+      }
+       else {
+        post.push(value);
+      }
+      exp = post.join("");
+      display(exp);
+    }
+  } else {
+    exp = "Maximum Limit Reached";
+    display(exp);
+    if (value === "AC") {
+      exp = "";
+      post = [];
+      display(exp);
+    }
+  }
+  console.log(post);
+}
+
+function calculation(pos) {
+  expression = pos.join("");
+  expression = expression.replace(/x/g, "*");
+  expression = expression.replace(/(\d+)%(\d+)/g, "($1 * ($2 / 100))");
+  expression = expression.replace(/--/g, "+");
+  expression = expression.replace(/\+\+/g, "+");
+  result = eval(expression);
+  return result;
+}
